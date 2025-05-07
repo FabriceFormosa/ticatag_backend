60906310b09ed32cd751141f1b09c59628612e01
FabriceFormosa <formosafabrice@wanadoo.fr>
2025-05-07 14:39:30 +0200

    ajout champ adresse postale

--- models/device.go
+++ models/device.go
@@ -3,9 +3,10 @@ package models
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type Device struct {
-	ID     primitive.ObjectID   `bson:"_id,omitempty" json:"id"`
-	Adress  string              `json:"adress" bson:"adress"`
-	Latitude  string 			`json:"latitude" bson:"latitude"`
-	Longitude string 			`json:"longitude" bson:"longitude"`
-	CreatedAt int64             `bson:"created_at" json:"created_at"`
+	ID            primitive.ObjectID `bson:"_id,omitempty" json:"id"`
+	Adress        string             `json:"adress" bson:"adress"`
+	Latitude      string             `json:"latitude" bson:"latitude"`
+	Longitude     string             `json:"longitude" bson:"longitude"`
+	Adresspostale string             `json:"addresspostale" bson:"adresspostale"`
+	CreatedAt     int64              `bson:"created_at" json:"created_at"`
 }
--- resources/device_response.go
+++ resources/device_response.go
@@ -5,20 +5,22 @@ import (
 )
 
 type DeviceResponse struct {
-	ID        string `bson:"_id,omitempty" json:"id"`
-	Adress    string `json:"adress" bson:"adress"`
-	Latitude  string `json:"latitude" bson:"latitude"`
-	Longitude string `json:"longitude" bson:"longitude"`
-	CreatedAt int64  `bson:"created_at" json:"created_at"`
+	ID            string `bson:"_id,omitempty" json:"id"`
+	Adress        string `json:"adress" bson:"adress"`
+	Latitude      string `json:"latitude" bson:"latitude"`
+	Longitude     string `json:"longitude" bson:"longitude"`
+	Adresspostale string `json:"addresspostale" bson:"adresspostale"`
+	CreatedAt     int64  `bson:"created_at" json:"created_at"`
 }
 
 func NewDeviceResponse(device models.Device) DeviceResponse {
 	return DeviceResponse{
-		ID:        device.ID.Hex(),
-		Adress:    device.Adress,
-		Latitude:  device.Latitude,
-		Longitude: device.Longitude,
-		CreatedAt: device.CreatedAt,
+		ID:            device.ID.Hex(),
+		Adress:        device.Adress,
+		Latitude:      device.Latitude,
+		Longitude:     device.Longitude,
+		Adresspostale: device.Adresspostale,
+		CreatedAt:     device.CreatedAt,
 	}
 }
 
